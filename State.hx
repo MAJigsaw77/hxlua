2ef32729ea83aba1c819763e49f2f0f2c445f224
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-02-24 15:56:14 +0200

    Create State.hx

---
+++ State.hx
@@ -0,0 +1,9 @@
+package lua;
+
+typedef State = cpp.RawPointer<LuaState>;
+
+@:buildXml("<include name='${haxelib:hxlua}/project/Build.xml' />")
+@:include("lua.hpp")
+@:keep
+@:native("luaState")
+extern class LuaState {}
