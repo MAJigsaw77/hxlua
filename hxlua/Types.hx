3f953f0d9abd0335e91dd731f0c19678eb4a2919
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-02-24 23:12:11 +0200

    Update Types.hx

--- hxlua/Types.hx
+++ hxlua/Types.hx
@@ -34,25 +34,25 @@ extern class Lua_KContext {}
 /*
  * Type for C functions registered with Lua
  */
-typedef Lua_CFunction = cpp.Callable<(L:cpp.Pointer<Lua_State>) -> Int>;
+typedef Lua_CFunction = cpp.Callable<(L:cpp.RawPointer<Lua_State>) -> Int>;
 
 /*
  * Type for continuation functions
  */
-typedef Lua_KFunction = cpp.Callable<(L:cpp.Pointer<Lua_State>, status:Int, ctx:Lua_KContext) -> Int>;
+typedef Lua_KFunction = cpp.Callable<(L:cpp.RawPointer<Lua_State>, status:Int, ctx:Lua_KContext) -> Int>;
 
 /*
  * Type for functions that read/write blocks when loading/dumping Lua chunks
  */
-typedef Lua_Reader = cpp.Callable<(L:cpp.Pointer<Lua_State>, ud:cpp.Pointer<cpp.Void>, sz:cpp.Pointer<cpp.SizeT>) -> String>;
-typedef Lua_Writer = cpp.Callable<(L:cpp.Pointer<Lua_State>, p:String, sz:cpp.SizeT, ud:cpp.Pointer<cpp.Void>) -> String>;
+typedef Lua_Reader = cpp.Callable<(L:cpp.RawPointer<Lua_State>, ud:cpp.Star<cpp.Void>, sz:cpp.Star<cpp.SizeT>) -> String>;
+typedef Lua_Writer = cpp.Callable<(L:cpp.RawPointer<Lua_State>, p:String, sz:cpp.SizeT, ud:cpp.Star<cpp.Void>) -> String>;
 
 /*
  * Type for memory-allocation functions
  */
-typedef Lua_Alloc = cpp.Callable<(ud:cpp.Pointer<cpp.Void>, ptr:cpp.Pointer<cpp.Void>, osize:cpp.SizeT, nsize:cpp.SizeT) -> cpp.RawPointer<cpp.Void>>;
+typedef Lua_Alloc = cpp.Callable<(ud:cpp.Star<cpp.Void>, ptr:cpp.Star<cpp.Void>, osize:cpp.SizeT, nsize:cpp.SizeT) -> cpp.Star<cpp.Void>>;
 
 /*
  * Type for warning functions
  */
-typedef Lua_WarnFunction = cpp.Callable<(L:ud:cpp.Pointer<cpp.Void>, msg:String, tocont:Int) -> Void>;
+typedef Lua_WarnFunction = cpp.Callable<(L:ud:cpp.Star<cpp.Void>, msg:String, tocont:Int) -> Void>;
