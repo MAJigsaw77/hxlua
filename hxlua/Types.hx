74c2b47198387e6e5be1670bc27a4eecd2b3ad57
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-02-26 22:36:16 +0200

    Update Types.hx

--- hxlua/Types.hx
+++ hxlua/Types.hx
@@ -32,7 +32,7 @@ typedef Lua_Writer = cpp.Callable<(L:cpp.RawPointer<Lua_State>, p:cpp.ConstStar<
 /*
  * Type for memory-allocation functions
  */
-typedef Lua_Alloc = cpp.Callable<(ud:cpp.Star<cpp.Void>, ptr:cpp.Star<cpp.Void>, osize:cpp.SizeT, nsize:cpp.SizeT) -> cpp.Star<cpp.Void>>;
+typedef Lua_Alloc = cpp.Callable<(cpp.Star<cpp.Void>, ptr:cpp.Star<cpp.Void>, osize:cpp.SizeT, nsize:cpp.SizeT) -> cpp.Star<cpp.Void>>;
 
 /*
  * Type for warning functions
