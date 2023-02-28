2e05e2a57aaa51d8deff2f104a1e73dea1c7d4d2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-02-28 07:59:00 +0200

    Update LuaOpen.hx

--- hxlua/LuaOpen.hx
+++ hxlua/LuaOpen.hx
@@ -8,36 +8,6 @@ import hxlua.Types;
 @:unreflective
 extern class LuaOpen
 {
-	@:native('LUA_VERSUFFIX')
-	static var VERSUFFIX:String;
-
-	@:native('LUA_COLIBNAME')
-	static var COLIBNAME:String;
-
-	@:native('LUA_TABLIBNAME')
-	static var TABLIBNAME:String;
-
-	@:native('LUA_IOLIBNAME')
-	static var IOLIBNAME:String;
-
-	@:native('LUA_OSLIBNAME')
-	static var OSLIBNAME:String;
-
-	@:native('LUA_STRLIBNAME')
-	static var STRLIBNAME:String;
-
-	@:native('LUA_UTF8LIBNAME')
-	static var UTF8LIBNAME:String;
-
-	@:native('LUA_MATHLIBNAME')
-	static var MATHLIBNAME:String;
-
-	@:native('LUA_DBLIBNAME')
-	static var DBLIBNAME:String;
-
-	@:native('LUA_LOADLIBNAME')
-	static var LOADLIBNAME:String;
-
 	@:native('luaopen_base')
 	static function base(L:cpp.RawPointer<Lua_State>):Int;
 
