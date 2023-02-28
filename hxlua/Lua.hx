33610b5aebda5ea598082966594257d472994977
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-02-28 07:41:15 +0200

    Update Lua.hx

--- hxlua/Lua.hx
+++ hxlua/Lua.hx
@@ -491,4 +491,45 @@ extern class Lua
 
 	@:native('lua_closeslot')
 	static function closeslot(L:cpp.RawPointer<Lua_State>, idx:Int):Void;
+
+	/*
+	 * some useful macros
+	 */
+	@:native('lua_getextraspace')
+	static function getextraspace(L:cpp.RawPointer<Lua_State>):cpp.Star<cpp.Void>;
+
+	@:native('lua_tonumber')
+	static function tonumber(L:cpp.RawPointer<Lua_State>, idx:Int):Float;
+
+	@:native('lua_tointeger')
+	static function tointeger(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
+
+	@:native('lua_newtable')
+	static function newtable(L:cpp.RawPointer<Lua_State>):Void;
+
+	@:native('lua_register')
+	static function register(L:cpp.RawPointer<Lua_State>, name:String, f:Lua_CFunction):Void;
+/*
+** {==============================================================
+** some useful macros
+** ===============================================================
+
+
+#define lua_pushcfunction(L,f)	lua_pushcclosure(L, (f), 0)
+#define lua_isfunction(L,n)	(lua_type(L, (n)) == LUA_TFUNCTION)
+#define lua_istable(L,n)	(lua_type(L, (n)) == LUA_TTABLE)
+#define lua_islightuserdata(L,n)	(lua_type(L, (n)) == LUA_TLIGHTUSERDATA)
+#define lua_isnil(L,n)		(lua_type(L, (n)) == LUA_TNIL)
+#define lua_isboolean(L,n)	(lua_type(L, (n)) == LUA_TBOOLEAN)
+#define lua_isthread(L,n)	(lua_type(L, (n)) == LUA_TTHREAD)
+#define lua_isnone(L,n)		(lua_type(L, (n)) == LUA_TNONE)
+#define lua_isnoneornil(L, n)	(lua_type(L, (n)) <= 0)
+#define lua_pushliteral(L, s)	lua_pushstring(L, "" s)
+#define lua_pushglobaltable(L)  \
+	((void)lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS))
+#define lua_tostring(L,i)	lua_tolstring(L, (i), NULL)
+#define lua_insert(L,idx)	lua_rotate(L, (idx), 1)
+#define lua_remove(L,idx)	(lua_rotate(L, (idx), -1), lua_pop(L, 1))
+#define lua_replace(L,idx)	(lua_copy(L, -1, (idx)), lua_pop(L, 1))*/
+
 }
