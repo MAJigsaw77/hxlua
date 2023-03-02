7b9460928fa3399ec4c272f0fb040eda4078d682
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-02 11:33:45 +0200

    Update LuaL.hx

--- hxlua/LuaL.hx
+++ hxlua/LuaL.hx
@@ -2,7 +2,7 @@ package hxlua;
 
 import hxlua.Types;
 
-@:buildXml("<include name='${haxelib:hxlua}/project/Build.xml' />")
+// @:buildXml("<include name='${haxelib:hxlua}/project/Build.xml' />")
 @:include("lua.hpp")
 @:keep
 @:unreflective
