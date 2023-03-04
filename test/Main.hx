1a6f0b894d4e9164fafb717f1148a09cf92ce637
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-04 22:56:18 +0200

    Update Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -10,6 +10,17 @@ class Main
 	{
 		var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
 		LuaL.openlibs(vm);
-		trace("Lua Version: " + Lua.version(vm));
+		LuaL.dofile(vm, "script.lua");
+
+		trace(Lua.RELEASE);
+		trace(Lua.COPYRIGHT);
+		trace(Lua.AUTHORS);
+
+		Lua.getglobal(vm, "foo");
+		Lua.pushinteger(vm, 1);
+		Lua.pushnumber(vm, 2.0);
+		Lua.pushstring(vm, "three");
+		Lua.pcall(vm, 3, 0, 1);
+		Lua.close(vm);
 	}
 }
