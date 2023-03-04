1a09cf2f7d5fbccab6728af617cfc9f5a1de3acf
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-04 18:24:00 +0200

    Update Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -2,13 +2,14 @@ package;
 
 import hxlua.Lua;
 import hxlua.LuaL;
-import hxlua.LuaOpen;
+inport hxlua.Types;
 
 class Main
 {
 	public static function main():Void
 	{
-		trace('hmm');
-		trace(Lua.VERSION_NUM + Lua.VERSION_RELEASE_NUM);
+		var lua:cpp.RawPointer<Lua_State> = LuaL.newstate();
+		LuaL.openlibs(lua);
+		trace("Lua Version: " + Lua.version(lua));
 	}
 }
