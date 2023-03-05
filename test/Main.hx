02cec695fadabbcda1ee9b197a458765791f2323
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-05 20:43:50 +0200

    Update Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -8,25 +8,36 @@ class Main
 {
 	public static function main():Void
 	{
+		// some informations
+
 		trace(Lua.RELEASE);
 		trace(Lua.COPYRIGHT);
 		trace(Lua.AUTHORS);
 
+		// creating the new state
 		var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
+
+		// open the libs
 		LuaL.openlibs(vm);
+
+		// do the file
 		var ret:Int = LuaL.dofile(vm, "script.lua");
 
+		// check if is ok
 		if (ret != Lua.OK)
 		{
 			trace("Lua Error: " + Lua.tostring(vm, ret));
 			Lua.pop(vm, 1);
 		}
 
+		// calling 'foo' function
 		Lua.getglobal(vm, "foo");
 		Lua.pushinteger(vm, 1);
 		Lua.pushnumber(vm, 2.0);
 		Lua.pushstring(vm, "three");
 		Lua.pcall(vm, 3, 0, 1);
+
+		// close the state after pcall
 		Lua.close(vm);
 		vm = null;
 	}
