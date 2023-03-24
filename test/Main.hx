017ff31f8ff7bcb13bbb89cda378cb13b55beec3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-24 22:23:02 +0200

    Update Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -6,37 +6,36 @@ import hxlua.Types;
 
 class Main
 {
-	public static function main():Void
-	{
-		// create the new state
-		var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
+  public static function main():Void
+  {
+    // create the new state
+    var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
 
-		// open the libs
-		LuaL.openlibs(vm);
+    // open the libs
+    LuaL.openlibs(vm);
 
-		// do the file
-		var ret:Int = LuaL.dofile(vm, "script.lua");
+    // do the file
+    var ret:Int = LuaL.dofile(vm, "script.lua");
 
-		// check if isn't ok
-		if (ret != Lua.OK)
-		{
-			trace("Lua Error: " + Lua.tostring(vm, ret));
-			Lua.pop(vm, 1);
-		}
+    // check if isn't ok
+    if (ret != Lua.OK)
+    {
+      trace("Lua Error: " + Lua.tostring(vm, ret));
+      Lua.pop(vm, 1);
+    }
 
-		// call 'foo' function
-		Lua.getglobal(vm, "foo");
-		Lua.pushinteger(vm, 1);
-		Lua.pushnumber(vm, 2.0);
-		Lua.pushstring(vm, "three");
-		Lua.pcall(vm, 3, 0, 1);
+    // call 'foo' function
+    Lua.getglobal(vm, "foo");
+    Lua.pushinteger(vm, 1);
+    Lua.pushnumber(vm, 2.0);
+    Lua.pushstring(vm, "three");
+    Lua.pcall(vm, 3, 0, 1);
 
-		// getting the gc memory count after the call
-		var count:Int = Lua.gc(vm, Lua.GCCOUNT, [0]); // rest args are in the array...
-		trace('Lua GC Memory: $count KB');
+    // getting the gc memory count after the call
+    trace('Lua GC Memory: ${Lua.gc(vm, Lua.GCCOUNT, [])} KB');
 
-		// close the state
-		Lua.close(vm);
-		vm = null;
-	}
+    // close the state
+    Lua.close(vm);
+    vm = null;
+  }
 }
