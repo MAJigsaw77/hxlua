e7160030366faa32b2d17988e4380e2c18b74b4f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-27 20:12:55 +0300

    Create Main.hx

---
+++ samples/callback/Main.hx
@@ -0,0 +1,58 @@
+package;
+
+import hxlua.Lua;
+import hxlua.LuaL;
+import hxlua.Types;
+
+class Main
+{
+  public static function main():Void
+  {
+    /* initialize Lua */
+    var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
+
+    /* load Lua base libraries */
+    LuaL.openlibs(vm);
+
+    /* register our function */
+    Lua.register(vm, "average", cpp.Function.fromStaticFunction(average));
+
+    /* run the script */
+    LuaL.dofile(vm, "script.lua");
+
+    /* cleanup Lua */
+    Lua.close(vm);
+    vm = null;
+  }
+
+  private static function average(l:cpp.RawPointer<Lua_State>):Int
+  {
+    var n:Int = Lua.gettop(l);
+    var sum:Float = 0;
+
+    /* loop through each argument */
+    for (i in 0...n)
+    {
+        if (Lua.isnumber(l, i) != 1)
+        {
+            Lua.pushstring(l, "Incorrect argument to 'average'");
+            Lua.error(l);
+        }
+
+	/* total the arguments */
+	sum += Lua.tonumber(l, i);
+    }
+
+    /* clear the stack */
+    Lua.pop(l, n);
+
+    /* push the average */
+    Lua.pushnumber(l, sum / n);
+
+    /* push the sum */
+    Lua.pushnumber(l, sum);
+
+    /* return the number of results */
+    return 2;
+  }
+}
