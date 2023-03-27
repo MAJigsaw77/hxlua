531b0f1816a56ff97fe732e367521f4af10dc088
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-27 22:13:24 +0300

    Create Main.hx

---
+++ samples/pass-variable/Main.hx
@@ -0,0 +1,27 @@
+package;
+
+import hxlua.Lua;
+import hxlua.LuaL;
+import hxlua.Types;
+
+class Main
+{
+  public static function main():Void
+  {
+    /* initialize Lua */
+    var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
+
+    /* load Lua base libraries */
+    LuaL.openlibs(vm);
+
+    Lua.pushstring(vm, "0.9");
+    Lua.setglobal(vm, "VERSION");
+
+    /* run the script */
+    LuaL.dofile(vm, "script.lua");
+
+    /* cleanup Lua */
+    Lua.close(vm);
+    vm = null;
+  }
+}
