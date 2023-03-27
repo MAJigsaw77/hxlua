a33783c3178b4f7a6726bdbc4806e488073ae9ce
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-27 19:46:47 +0300

    Create script.lua

---
+++ samples/alter-state/script.lua
@@ -0,0 +1,10 @@
+-- The variables are set by default
+print("apple", apple)
+print("banana", banana)
+print("cherry", cherry)
+
+-- The values are remembered
+-- (they're attached to the state)
+apple = apple + 10
+banana = banana * 10
+cherry = cherry - 3
