1a78bd42ebd2be5f32c42889b3eac19f919af566
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-27 22:01:18 +0300

    Create Main.hx

---
+++ samples/override/Main.hx
@@ -0,0 +1,40 @@
+package;
+
+import hxlua.Lua;
+import hxlua.LuaL;
+import hxlua.Types;
+
+class Main
+{
+  public static function main():Void
+  {
+    /* initialize Lua */
+    var vm:cpp.RawPointer<Lua_State> = LuaL.newstate();
+
+    /* load Lua base libraries */
+    LuaL.openlibs(vm);
+
+    /* register our function */
+    Lua.register(vm, "print", cpp.Function.fromStaticFunction(hx_print));
+
+    /* run the script */
+    LuaL.dofile(vm, "script.lua");
+
+    /* cleanup Lua */
+    Lua.close(vm);
+    vm = null;
+  }
+
+  private static function hx_print(l:cpp.RawPointer<Lua_State>):Int
+  {
+    var n:Int = Lua.gettop(l);
+
+    /* loop through each argument */
+    for (i in 0...n)
+      Sys.printIn(Lua.tolstring(l, i, null));
+
+    /* clear the stack */
+    Lua.pop(l, n);
+    return 0;
+  }
+}
