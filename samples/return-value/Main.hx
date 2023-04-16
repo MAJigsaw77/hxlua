3a4fff7183071652c502839fc5589b4f0d6e7f2d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-16 23:18:42 +0300

    Update Main.hx

--- samples/return-value/Main.hx
+++ samples/return-value/Main.hx
@@ -20,11 +20,8 @@ class Main
 		/* run the script */
 		LuaL.dofile(vm, "script.lua");
 
-		var stack:Int = Lua.gettop(vm);
-		trace('stack: $stack');
-
-		var num:Int = Lua.tointeger(vm, 1);
-		trace('num: $num');
+		trace('stack: ${Lua.gettop(vm)}');
+		trace('num: ${Lua.tointeger(vm, 1)}');
 
 		/* cleanup Lua */
 		Lua.close(vm);
