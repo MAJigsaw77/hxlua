ff65d2879c6cfb36669fd17a62630f508aed6bd5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-30 11:57:03 +0300

    A little shit

--- examples/simple-call/Main.hx
+++ examples/simple-call/Main.hx
@@ -6,6 +6,11 @@ import hxlua.Types;
 
 class Main
 {
+	private static function kshit(L:cpp.RawPointer<Lua_State>, status:Int, ctx:Lua_KContext):Int
+	{
+		return 0;
+	}
+
 	public static function main():Void
 	{
 		/* version info */
@@ -19,6 +24,8 @@ class Main
 
 		/* run the script */
 		LuaL.dofile(vm, "script.lua");
+
+		Lua.yieldk(vm, 0, 0, cpp.Function.fromStaticFunction(kshit));
 
 		/* cleanup Lua */
 		Lua.close(vm);
