44b0edbbee9def9f6a453feadaffaaac51044845
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-01 10:44:20 +0300

    Ok

--- source/hxlua/Types.hx
+++ source/hxlua/Types.hx
@@ -3,30 +3,47 @@ package hxlua;
 #if !cpp
 #error 'Lua supports only C++ target platforms.'
 #end
-
-class Types {} // blank
+class Types {}
 
 @:buildXml('<include name="${haxelib:hxlua}/project/Build.xml" />')
 @:include('lua.hpp')
 @:native('lua_State')
 extern class Lua_State {}
 
-/*
+/**
  * Type of numbers in Lua.
  */
-typedef Lua_Number = Float;
+@:buildXml('<include name="${haxelib:hxlua}/project/Build.xml" />')
+@:include('lua.hpp')
+@:native('lua_Number')
+@:scalar
+@:coreType
+@:notNull
+extern abstract Lua_Number from Float to Float {}
 
-/*
- * Type for integer functions 
+/**
+ * Type for integer functions
  */
-typedef Lua_Integer = cpp.Int64;
+@:buildXml('<include name="${haxelib:hxlua}/project/Build.xml" />')
+@:include('lua.hpp')
+@:native('lua_Integer')
+@:scalar
+@:coreType
+@:notNull
+extern abstract Lua_Integer from cpp.Int64 to cpp.Int64 {}
 
-/*
+/**
  * Type of unsigned integer.
  */
-typedef Lua_Unsigned = cpp.UInt64;
+@:buildXml('<include name="${haxelib:hxlua}/project/Build.xml" />')
+@:include('lua.hpp')
+@:native('lua_Unsigned')
+@:scalar
+@:coreType
+@:notNull
+extern abstract Lua_Unsigned from cpp.UInt64 to cpp.UInt64 {}
 
-/*
+/**
  * Type for continuation-function contexts.
  */
 @:buildXml('<include name="${haxelib:hxlua}/project/Build.xml" />')
@@ -37,37 +54,37 @@ typedef Lua_Unsigned = cpp.UInt64;
 @:notNull
 extern abstract Lua_KContext from Int to Int {}
 
-/*
+/**
  * Type for C functions registered with Lua.
  */
 typedef Lua_CFunction = cpp.Callable<(L:cpp.RawPointer<Lua_State>) -> Int>;
 
-/*
+/**
  * Type for continuation functions.
  */
 typedef Lua_KFunction = cpp.Callable<(L:cpp.RawPointer<Lua_State>, status:Int, ctx:Lua_KContext) -> Int>;
 
-/*
+/**
  * Type for functions that read blocks when loading/dumping Lua chunks.
  */
 typedef Lua_Reader = cpp.Callable<(L:cpp.RawPointer<Lua_State>, ud:cpp.RawPointer<cpp.Void>, sz:cpp.RawPointer<cpp.SizeT>) -> cpp.ConstCharStar>;
 
-/*
+/**
  * Type for functions that write blocks when loading/dumping Lua chunks.
  */
 typedef Lua_Writer = cpp.Callable<(L:cpp.RawPointer<Lua_State>, p:cpp.RawConstPointer<cpp.Void>, sz:cpp.SizeT, ud:cpp.RawPointer<cpp.Void>) -> Int>;
 
-/*
+/**
  * Type for memory-allocation functions.
  */
 typedef Lua_Alloc = cpp.Callable<(ud:cpp.RawPointer<cpp.Void>, ptr:cpp.RawPointer<cpp.Void>, osize:cpp.SizeT, nsize:cpp.SizeT) -> cpp.RawPointer<cpp.Void>>;
 
-/*
+/**
  * Type for warning functions.
  */
 typedef Lua_WarnFunction = cpp.Callable<(ud:cpp.RawPointer<cpp.Void>, msg:cpp.ConstCharStar, tocont:Int) -> Void>;
 
-/*
+/**
  * Type for debugging hook functions.
  */
 typedef Lua_Hook = cpp.Callable<(L:cpp.RawPointer<Lua_State>, ar:cpp.RawPointer<Lua_Debug>) -> Void>;
